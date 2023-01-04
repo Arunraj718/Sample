99867341f33c98d0bf24aad031b3f6730085ce26
Arun Balamurugan <arun.bk718@gmail.com>
2023-01-04 21:31:51 +0530

    message

---
+++ src/test/java/com/git/Gittask.java
@@ -0,0 +1,20 @@
+package com.git;
+
+public class Gittask {
+
+	
+	public void Cricket() {
+		System.out.println("Dhoni");
+		
+	}
+	public void Football() {
+		System.out.println("Ronaldo");
+	}
+	public void Basketball() {
+		System.out.println("James");
+	}
+
+	
+	
+	
+}
